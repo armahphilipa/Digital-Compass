d85e81b9c75a7258ef6a1c21099cb9306e307272
armahphilipa <130850530+armahphilipa@users.noreply.github.com>
2024-11-14 14:31:10 +0000

    Update contract.js

--- main/contract.js
+++ main/contract.js
@@ -3,12 +3,15 @@ function acceptTerms() {
   const confirmationMessage = document.getElementById('confirmationMessage');
 
   if (checkbox.checked) {
-    confirmationMessage.textContent = "Thank you! You have accepted the terms and conditions.";
-    <br>
+    confirmationMessage.innerHTML = `
+      Thank you! You have accepted the terms and conditions.
+      <br>
       <a href="index.html" class="home-button">Go to Home</a>
+    `;
     confirmationMessage.style.color = "#4caf50";
   } else {
     confirmationMessage.textContent = "Please check the box to accept the terms and conditions.";
     confirmationMessage.style.color = "red";
   }
-};
+}
+
