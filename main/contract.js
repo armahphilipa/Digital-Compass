a4e7318878108f88a919fb149813a9dadbad63f8
armahphilipa <130850530+armahphilipa@users.noreply.github.com>
2024-11-14 14:24:50 +0000

    Update contract.js

--- main/contract.js
+++ main/contract.js
@@ -4,6 +4,8 @@ function acceptTerms() {
 
   if (checkbox.checked) {
     confirmationMessage.textContent = "Thank you! You have accepted the terms and conditions.";
+    <br>
+      <a href="index.html" class="home-button">Go to Home</a>
     confirmationMessage.style.color = "#4caf50";
   } else {
     confirmationMessage.textContent = "Please check the box to accept the terms and conditions.";
